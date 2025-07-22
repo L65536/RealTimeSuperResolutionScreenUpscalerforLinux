716e17df06500669e4ce80058a2dc5f0df4be0a6
L65536 <111718831+L65536@users.noreply.github.com>
2025-07-22 19:13:19 +1000

    Add files via upload

---
+++ v02/LIBCAP_LINUX.py
@@ -0,0 +1,58 @@
+import Xlib # capture and window location
+import Xlib.display
+from Xlib import X
+
+def enumerate_window_property(): # enum window ID width height title into windows=[{dict}]
+    global windows
+    windows = []
+    disp = Xlib.display.Display()
+    try:
+        disp_root = disp.screen().root
+        windowIDs = disp_root.get_full_property(disp.intern_atom('_NET_CLIENT_LIST'), X.AnyPropertyType).value
+        for windowID in windowIDs:
+            window = disp.create_resource_object('window', windowID)
+            wproperty = window.get_full_property(disp.intern_atom('_NET_WM_NAME'), 0)
+            geometry = window.get_geometry()
+            title = wproperty.value.decode('utf-8') #.lower()
+            title_words = title.split()
+            if title_words: title = title_words[-1]
+            if 'RTSR' in title: continue # skip self
+            if 'Desktop' in title: continue # skip
+            if 'panel' in title: continue # skip
+            if DEBUG: print(f"{windowID} ({geometry.width:<4} x {geometry.height:<4}) {title}")
+            windows.append({"id":windowID, "width":geometry.width, "height":geometry.height, "title":title, "accessible": 0})
+    finally:
+        disp.close()
+        
+    return windows    
+
+def capture_init(hwnd): # create capture handles only once
+    # global WIN_HANDLES
+    # WIN_HANDLES = (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap)
+    
+    disp = Xlib.display.Display() # MOVE INIT OUTSIDE LOOP
+    window = disp.create_resource_object('window', winID)
+    geometry = window.get_geometry() # geometry.width geometry.height # need to re-init shader if dimension changes
+    width, height = geometry.width, geometry.height
+    # height = winH
+    # width = winW       
+    return (disp, window, width, height)
+                
+def capture():
+    # global WIN_HANDLES
+    # (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap) = WIN_HANDLES
+    (disp, window, width, height) = WIN_HANDLES
+    
+    pixmap = window.get_image(0, 0, width, height, X.ZPixmap, 0xffffffff) # will fail if minimized # DEBUG print(len(pixmap.data))
+    # image = Image.frombuffer('RGBX', (width, height), pixmap.data, "raw", "BGRX") # must use RGBX, RGBA not working in some cases ??? PIL LINUX
+    # surface = pygame.image.frombuffer(pixmap.data, (width,height), "BGRX") # Windows pygame
+    # buffer = pixmap.data                       
+    return pixmap.data, width, height
+    # return bmpstr, bmpinfo["bmWidth"], bmpinfo["bmHeight"] # Windows
+
+def capture_release(WIN_HANDLES):
+    # global WIN_HANDLES
+    # (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap) = WIN_HANDLES
+    (disp, window, width, height) = WIN_HANDLES
+    disp.close()    
+    window.close() # ???
---
+++ v02/LIBCAP_WINDOWS.py
@@ -0,0 +1,73 @@
+import win32gui # pip install pywin32
+import win32ui
+from win32.win32gui import FindWindow, GetWindowRect, GetForegroundWindow, GetWindowText
+from ctypes import windll
+windll.user32.SetProcessDPIAware()
+
+def enumerate_window_property(): # Retrieves a list of all visible top-level window handles.
+    # global windows
+    windows = []
+    handles = []
+    def enum_windows_callback(hwnd, lParam): # Callback function for EnumWindows. Appends visible window handles to the list.
+        if win32gui.IsWindowVisible(hwnd): handles.append(hwnd)
+        return True  # Continue enumeration
+    win32gui.EnumWindows(enum_windows_callback, None)
+
+    for handle in handles:
+        try:
+            title = win32gui.GetWindowText(handle)
+            left, top, right, bottom = win32gui.GetClientRect(handle)
+            w = right - left
+            h = bottom - top
+            if w<60 or h<60: # ignore minimized window
+                print(f"Skip minimized or inaccessible windows: [{handle:<8}]({w} x {h})'{title}'")
+                continue
+            # if w==3840: continue # ignore full screen windows
+            if title:  # Only print handles with a visible title
+                title_words = title.split()
+                if title_words: title = title_words[-1] # trim title string to the last word
+                # print(f"[{handle:<8}]({w:<4} x {h:<4})'{title}'") # print(w, h, "=", left, top, right, bottom)
+                windows.append({"id":handle, "width":w, "height":h, "title":title, "accessible": 0})
+        except Exception as e:
+            print(f"Could not get title for handle {handle}: {e}")
+
+    for i in range(len(windows)):
+        print(f"[{windows[i]['id']:<8}]({windows[i]['width']:<4} x {windows[i]['height']:<4})'{windows[i]['title']}'")
+    print()
+    return windows
+    
+def init(hwnd): # create capture handles only once
+    # global WIN_HANDLES
+    windll.user32.SetProcessDPIAware()
+    left, top, right, bottom = win32gui.GetClientRect(hwnd)
+    w = right - left
+    h = bottom - top
+    #w = winW # w = right - left # use trimmed/padded sizes for shaders
+    #h = winH # h = bottom - top  # use trimmed/padded sizes for shaders
+    hwnd_dc = win32gui.GetWindowDC(hwnd)
+    mfc_dc = win32ui.CreateDCFromHandle(hwnd_dc)
+    save_dc = mfc_dc.CreateCompatibleDC()
+    bitmap = win32ui.CreateBitmap()
+    bitmap.CreateCompatibleBitmap(mfc_dc, w, h)
+    return (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap)
+    # WIN_HANDLES = (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap)
+    
+def get(WIN_HANDLES):
+    # global WIN_HANDLES
+    (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap) = WIN_HANDLES
+    save_dc.SelectObject(bitmap)
+    result = windll.user32.PrintWindow(hwnd, save_dc.GetSafeHdc(), 3)
+    if result != 1: raise RuntimeError(f"Unable to acquire screenshot! Result: {result}")
+
+    bmpinfo = bitmap.GetInfo()
+    bmpstr = bitmap.GetBitmapBits(True)
+    return bmpstr, bmpinfo["bmWidth"], bmpinfo["bmHeight"]
+    
+def release(WIN_HANDLES):
+    # global WIN_HANDLES
+    (hwnd, hwnd_dc, mfc_dc, save_dc, bitmap) = WIN_HANDLES
+    win32gui.DeleteObject(bitmap.GetHandle())
+    save_dc.DeleteDC()
+    mfc_dc.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwnd_dc)
+    WIN_HANDLES = None    
